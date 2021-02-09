442c34c6d9a930505869e539f73375fd7686181e
adawi <itaf790>
2021-02-10 00:54:00 +0200

    ok

---
+++ login.js
@@ -0,0 +1,11 @@
+function login(){
+
+var username= document.getElementById("username").value;
+var password= document.getElementById("password").value;
+
+if(username=="sandy" && password=="12"){
+
+
+}
+
+}
